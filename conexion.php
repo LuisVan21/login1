b977386c8e54b13ce1b9c5828e88008b43597c7e
[Luis Garcia] <[softproject@gmail.com]>
2024-08-27 11:51:30 -0700

    se hicieron ajustes en la conexion a DDBB

--- conexion.php
+++ conexion.php
@@ -9,11 +9,7 @@
         $pdo= new PDO('mysql:host='.$server.';dbname='.$DDBB, $user, $pass );
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $sql=" INSERT INTO `usuarios` (`id`, `nombres`, `apellidos`, `email`, `password`, `genero`, `curso`) 
-        VALUES (NULL, 'Luis', 'Garcia', 'luis@ejemplo.com', '12345', 'Hombre', 'php');";
-
-        $rst= $pdo->prepare($sql);
-        $rst->execute();
+        
 
     }catch(PDOException $e){
         echo "hubo un error de conexion" . $e->getMessage();
