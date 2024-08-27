fcaa4c49b8ad0889542a6f55509036e87276419b
[Luis Garcia] <[softproject@gmail.com]>
2024-08-27 08:38:26 -0700

    se agrego contenido a index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <a href="https://cursos.develoteca.com">Plataforma Educativa</a>
+    <a href="https://www.youtube.com/develoteca"></a>
+    <a href="cerrar.php"></a>
+
+    <h2>Inicio de la aplicacion</h2>
+    <p>este es el inicio de la aplicacion</p>
+    
+</body>
+</html>
+
