2fedbd2c0c88fb44b3a2b80002f24b91a1234357
[Luis Garcia] <[softproject@gmail.com]>
2024-08-27 18:47:59 -0700

    se creo verificacion de password con BBDD

--- login.php
+++ login.php
@@ -4,20 +4,42 @@
 
         include("conexion.php");
 
-        echo '<pre>', var_dump($_POST), '</pre>';
+        //echo '<pre>', var_dump($_POST), '</pre>';
 
         $email=(isset($_POST['email']))?htmlspecialchars($_POST['email']):NULL;
         $password=(isset($_POST['password']))?$_POST['password']: NULL;
 
 
-        $sql= "SELECT  `email` FROM `usuarios` WHERE email=:email ";
+        $sql= "SELECT * FROM `usuarios` WHERE email=:email ";
         $sentencia= $pdo->prepare($sql); 
 
         $sentencia->execute(['email'=>$email]);
 
-        $usuario=$sentencia->fetch(PDO::FETCH_ASSOC);
+        $usuarios=$sentencia->fetchAll(PDO::FETCH_ASSOC);
 
-        echo '<pre>', var_dump($usuario), '</pre>';
+        $login= false;
+
+        foreach($usuarios as $user){
+            /*
+                if(password_verify($password,$user["password"])){
+                $login= true;
+            }
+            */
+
+            if($password ===$user["password"]){
+            $login= true;
+            }
+        }
+
+        if($login){
+            echo "Existe en DB";
+        }else{
+            echo "no existe en DB";
+        }
+
+        echo $login;
+
+        echo '<pre>', var_dump($login), '</pre>';
 
     }
 
