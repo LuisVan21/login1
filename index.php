d89a1c437bcdd296cad308f033070fb258359539
[Luis Garcia] <[softproject@gmail.com]>
2024-08-30 11:56:46 -0700

    give style to index.php file

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
     session_start();
     if(!isset($_SESSION["usuario_id"])){
         header("Location:logion.html");
-        exiit();
+        exit();
     }
 
-    echo '<pre>', var_dump($_SESSION), '</pre>';
+    // echo '<pre>', var_dump($_SESSION), '</pre>';
   
   ?>
 
@@ -16,20 +16,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/login_style.css">
     <title>Document</title>
 </head>
 <body>
 
-    <a href="https://cursos.develoteca.com">Plataforma Educativa</a>
-    |
-    <a href="https://www.youtube.com/develoteca">YouTube</a>
-    |
-    <a href="cerrar.php">Cerrar</a>
-    <br/>
-    <h2>Inicio de la aplicacion</h2>
-    <p>bienvenid@ <?php echo $_SESSION["usuario_nombre"]." ".$_SESSION["usuario_apellido"]; ?></p>
-    <p>este es el inicio de la aplicacion</p>
+    <nav class="menu">
+        <ul>
+        <a href="https://cursos.develoteca.com">Plataforma Educativa</a>
+        
+        <a href="https://www.youtube.com/develoteca">YouTube</a>
+        
+        <a href="cerrar.php">Cerrar</a>
+        </ul>
+    </nav>
+    <!-- usuario_apellido -->
     
+    <main class="main_container">
+    <h2 class="title">Inicio de la aplicacion</h2>
+    <p>bienvenid@: <span class="name_user"><?php echo " ".$_SESSION["usuario_apellido"]; ?></span></p>
+    <p class="app_start">este es el inicio de la aplicacion</p>
+    
+    </main>
 </body>
 </html>
 
